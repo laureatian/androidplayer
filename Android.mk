8d76ffe8481e4766504a08c2d624769e307e0196
Xu Guangxin <guangxin.xu@intel.com>
2017-03-09 02:34:22 -0500

    init android player

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/../common.mk
+
+LOCAL_SRC_FILES := \
+    androidplayer.cpp
+
+LOCAL_C_INCLUDES:= \
+        $(LOCAL_PATH)/.. \
+        external/libcxx/include \
+        $(TARGET_OUT_HEADERS)/libva
+
+LOCAL_SHARED_LIBRARIES := \
+        libutils \
+        liblog \
+        libc++ \
+        libva \
+        libva-android \
+        libgui \
+        libhardware \
+
+LOCAL_MODULE := androidplayer
+include $(BUILD_EXECUTABLE)
